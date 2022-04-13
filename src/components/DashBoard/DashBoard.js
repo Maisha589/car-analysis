d03b999df0c848bfa7d7d3dd017530104d40afeb
Maisha589 <maisharahman589@gmail.com>
2022-04-13 07:42:08 +0600

    pieChart

--- src/components/DashBoard/DashBoard.js
+++ src/components/DashBoard/DashBoard.js
@@ -5,11 +5,23 @@ import MyPieChart from '../MyPieChart/MyPieChart';
 const DashBoard = () => {
     return (
         <div className='bg-slate-200'>
-            <h2 className='text-4xl mb-3 p-5'>You can track our <span className='font-bold text-red-900 '>yearly</span> selling progress here...</h2>
+            <h2 className='text-4xl mb-3 p-5'>You can track our <span className='font-bold text-red-900 '>yearly</span> selling progress here.</h2>
             <div className='ml-20 p-8'>
                 <MyLineChart></MyLineChart>
+            </div >
+            <h2 className='text-4xl mb-3 mt-3 p-2'>You also can track our last five <span className='font-bold text-red-900 '>months</span> investment ratio.</h2>
+            <div className='flex'>
+                <div className='m-10'>
+                    <p className='text-2xl text-blue-500'>Blue: Dec-2021</p>
+                    <p className='text-2xl text-green-500 '>Green: Jan-2022</p>
+                    <p className='text-2xl text-yellow-500'>Yellow: Feb-2022</p>
+                    <p className='text-2xl text-red-400'>Red: March-2022</p>
+                    <p className='text-2xl text-orange-600'>Orange: April-2022</p>
+                </div>
+                <div >
+                    <MyPieChart></MyPieChart>
+                </div>
             </div>
-            <MyPieChart></MyPieChart>
         </div>
     );
 };
--- src/components/MyPieChart/MyPieChart.js
+++ src/components/MyPieChart/MyPieChart.js
@@ -1,10 +1,74 @@
 import React from 'react';
+import { Cell, Pie, PieChart, Tooltip } from 'recharts';
 
 const MyPieChart = () => {
+    const data = [
+        {
+            "month": "Dec",
+            "investment": 700000,
+            "sell": 690,
+            "targeted-sell": 500
+        },
+        {
+            "month": "jan",
+            "investment": 100000,
+            "sell": 300,
+            "targeted-sell": 600
+        },
+        {
+            "month": "feb",
+            "investment": 300000,
+            "sell": 450,
+            "targeted-sell": 600
+        },
+        {
+            "month": "Mar",
+            "investment": 200000,
+            "sell": 651,
+            "targeted-sell": 600
+        },
+        {
+            "month": "Apr",
+            "investment": 400000,
+            "sell": 700,
+            "targeted-sell": 600
+        }
+
+    ];
+
+    const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', "orange"];
+
+    const RADIAN = Math.PI / 180;
+    const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+        const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+        const x = cx + radius * Math.cos(-midAngle * RADIAN);
+        const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+        return (
+            <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+                {`${(percent * 100).toFixed(0)}%`}
+            </text>
+        );
+    };
+
     return (
-        <div>
-            <h2>This is pie chart</h2>
-        </div>
+        <PieChart className='mt-0 ml-20 mr-20' width={500} height={500}>
+            <Pie
+                data={data}
+                cx={"50%"}
+                cy={"50%"}
+                labelLine={false}
+                label={renderCustomizedLabel}
+                outerRadius={100}
+                fill="#8884d8"
+                dataKey={"investment"}
+            >
+                {data.map((entry, index) => (
+                    <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                ))}
+            </Pie>
+            <Tooltip></Tooltip>
+        </PieChart >
     );
 };
 
